3ed087aa71eaed76a5ce82af506b80a65a8c1e3f
Jeongha Im <yimjh3205@gmail.com>
2025-10-29 17:57:07 +0900

    Lv5 : 예외 처리 추가

--- src/level5/Kiosk.java
+++ src/level5/Kiosk.java
@@ -1,5 +1,6 @@
 package level5;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -27,47 +28,63 @@ public class Kiosk {
             System.out.println("0. 종료    | 종료");
 
             // 숫자 입력 받기
-            System.out.print("선택 : ");
-            int choice1 = sc.nextInt();
+            try {
+                System.out.print("선택 : ");
+                int choice1 = sc.nextInt();
 
-            // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
-            if (choice1 == 0) {
-                // 종료
-                System.out.println("프로그램을 종료합니다.");
-                running = false;
-            } else if (0 < choice1 && choice1 <= menus.size()) {
-                // 카테고리 선택
-                Menu selectCategory = menus.get(choice1 - 1);
+                // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+                if (choice1 == 0) {
+                    // 종료
+                    System.out.println("프로그램을 종료합니다.");
+                    running = false;
+                } else if (0 < choice1 && choice1 <= menus.size()) {
+                    // 카테고리 선택
+                    Menu selectCategory = menus.get(choice1 - 1);
+                    boolean running2 = true;
 
-                boolean running2 = true;
-                // [ 상세 메뉴 ] 출력하기
-                while (running2) {
-                    selectCategory.printMenuItem();
+                    // [ 상세 메뉴 ] 출력하기
+                    while (running2) {
+                        selectCategory.printMenuItem();
 
-                    System.out.print("선택 : ");
-                    int choice2 = sc.nextInt();
+                        try {
+                            System.out.print("선택 : ");
+                            int choice2 = sc.nextInt();
 
-                    if(choice2 == 0) {
-                        System.out.println("뒤로가기\n");
-                        running2 = false;
-                    } else if (0 < choice2 && choice2 <= selectCategory.getMenuItems().size()) {
-                        // selectCategory.getMenuItems() = 선택한 카테고리의 메뉴아이템 리스트
-                        MenuItem selectedItem = selectCategory.getMenuItems().get(choice2 - 1);
+                            if (choice2 == 0) {
+                                System.out.println("뒤로가기\n");
+                                running2 = false;
+                            } else if (0 < choice2 && choice2 <= selectCategory.getMenuItems().size()) {
+                                MenuItem selectedItem = selectCategory.getMenuItems().get(choice2 - 1);
 
-                        System.out.printf("선택한 메뉴 : %s | W %s | %s\n", selectedItem.getName(), selectedItem.getPrice(), selectedItem.getDescription());
-                        System.out.println();
-                        running2 = false;
-                    } else {
-                        System.out.println("메뉴 번호를 다시 확인해주세요.");
+                                System.out.printf("선택한 메뉴 : %s | W %s | %s\n", selectedItem.getName(), selectedItem.getPrice(), selectedItem.getDescription());
+                                System.out.println();
+                                running2 = false;
+                            }
+                            // 상세 : 범위를 벗어난 숫자 처리하기
+                            else {
+                                System.out.println("상세 메뉴 번호를 다시 확인해주세요.");
+                            }
+                        }
+                        // 상세 : 문자 입력시 처리하기
+                        catch (InputMismatchException e) {
+                            System.out.println("상세 메뉴 번호를 숫자로 선택해주세요.");
+                            sc.nextLine();
+                        }
+
                     }
-                    
                 }
-
+                // 메인 : 범위를 벗어난 숫자 처리하기
+                else {
+                    System.out.println("메인 메뉴 번호를 다시 확인해주세요.\n");
+                }
             }
-
-        } sc.close();
-
-
+            // 메인 : 문자 입력시 처리하기
+            catch (InputMismatchException e) {
+                System.out.println("메인 메뉴 번호를 숫자로 선택해주세요.\n");
+                sc.nextLine();
+            }
         }
-
+        sc.close();
     }
+}
+
