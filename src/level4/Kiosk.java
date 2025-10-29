4cd3aeed4163b9eb208498f8b5df570a7ad53270
Jeongha Im <yimjh3205@gmail.com>
2025-10-29 12:42:14 +0900

    Lv4 기능 구현 완료

---
+++ src/level4/Kiosk.java
@@ -0,0 +1,77 @@
+// Kiosk 클래스
+// Menu에 있는 카테고리를 불러와야함
+
+
+package level4;
+
+import java.util.*;
+
+public class Kiosk {
+    /*--------------속성---------------*/
+    List<Menu> menus;
+
+
+    /*--------------생성자---------------*/
+    public Kiosk(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    /*--------------기능---------------*/
+    void start() {
+        Scanner sc = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            // [ MAIN MENU ] 출력하기
+            System.out.println("[ MAIN MENU ]");
+            for (int i = 0; i < menus.size(); i++) {
+                menus.get(i).printCategory(i + 1);
+            }
+            System.out.println("0. 종료    | 종료");
+
+            // 숫자 입력 받기
+            System.out.print("선택 : ");
+            int choice1 = sc.nextInt();
+
+            // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+            if (choice1 == 0) {
+                // 종료
+                System.out.println("프로그램을 종료합니다.");
+                running = false;
+            } else if (0 < choice1 && choice1 <= menus.size()) {
+                // 카테고리 선택
+                Menu selectCategory = menus.get(choice1 - 1);
+
+
+                boolean running2 = true;
+                while (running2) {
+                    selectCategory.printMenuItem();
+
+                    System.out.print("선택 : ");
+                    int choice2 = sc.nextInt();
+
+                    if(choice2 == 0) {
+                        System.out.println("뒤로가기\n");
+                        running2 = false;
+                    } else if (0 < choice2 && choice2 <= selectCategory.getMenuItems().size()) {
+                        // selectCategory.getMenuItems() = 선택한 카테고리의 메뉴아이템 리스트
+                        MenuItem selectedItem = selectCategory.getMenuItems().get(choice2 - 1);
+
+                        System.out.printf("선택한 메뉴 : %s | W %s | %s\n", selectedItem.getName(), selectedItem.getPrice(), selectedItem.getDescription());
+                        System.out.println();
+                        running = false;
+                    } else {
+                        System.out.println("메뉴 번호를 다시 확인해주세요.");
+                    }
+                    
+                }
+
+            }
+
+        }
+
+
+
+        }
+
+    }
---
+++ src/level4/MenuItem.java
@@ -0,0 +1,28 @@
+package level4;
+
+public class MenuItem {
+    /*--------------속성---------------*/
+    private String name;
+    private String price;
+    private String description;
+
+    /*--------------생성자---------------*/
+    public MenuItem (String name, String price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    /*--------------기능---------------*/
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPrice() {
+        return this.price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
